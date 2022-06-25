3d018d35b0cba523e71c0916932295fed5238223
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 22:16:08 -0400

    finished numbers methods and test case corrections

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,36 +1,67 @@
 package io.zipcoder.microlabs.mastering_loops;
-
+import java.lang.Math.*;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String out = "";
+        for(int i = start; i < stop; i++) {
+            if(i % 2 == 0) {
+                out += Integer.toString(i);
+            }
+        }
+        return out;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String out = "";
+        for(int i = start; i < stop; i++) {
+            if(i % 2 != 0) {
+                out += Integer.toString(i);
+            }
+        }
+        return out;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String out = "";
+        for(int i = start; i < stop; i+=step) {
+            out += Integer.toString(i*i);
+        }
+        return out;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String out = "";
+        for(int i = 0; i < stop; i ++) {
+            out += i;
+        }
+        return out;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String out = "";
+        for(int i = start; i < stop; i++) {
+            out += i;
+        }
+        return out;
     }
-
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String out = "";
+        for(int i = start; i < stop; i+=step) {
+            out += i;
+        }
+        return out;
     }
 
-
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String out = "";
+        for(int i = start; i < stop; i+=step) {
+            int value = (int)Math.pow(i, exponent);
+            out += Integer.toString(value);
+        }
+        return out;
     }
 }
