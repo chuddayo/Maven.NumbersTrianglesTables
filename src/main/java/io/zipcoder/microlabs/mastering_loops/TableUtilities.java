4fa49ad9a521687b2fcd795656f4070cccb5e4fe
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 23:17:59 -0400

    finished all test cases

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,39 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
+    // 4 x 4 table
     public static String getSmallMultiplicationTable() {
-        return null;
+        String out = "";
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                out += String.format("%3d |", i*j);
+            }
+            out += "\n";
+        }
+        return out;
     }
 
+    // 10 x 10 table
     public static String getLargeMultiplicationTable() {
-        return null;
+        String out = "";
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                out += String.format("%3d |", i*j);
+            }
+            out += "\n";
+        }
+        return out;
     }
 
+    // tableSize table!
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String out = "";
+        for (int i = 1; i < tableSize + 1; i++) {
+            for (int j = 1; j < tableSize + 1; j++) {
+                out += String.format("%3d |", i*j);
+            }
+            out += "\n";
+        }
+        return out;
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,34 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String out = "";
+        for (int i = 1; i <= numberOfRows; i ++) {
+            out += getRow(i) + "\n";
+        }
+        return out;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String out = "";
+        for (int i = 0; i < numberOfStars; i++) {
+            out += "*";
+        }
+        return out;
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String out = "";
+        for (int i = 1; i <= 4; i ++) {
+            out += getRow(i) + "\n";
+        }
+        return out;
     }
 
     public static String getLargeTriangle() {
-        return null;
+        String out = "";
+        for (int i = 1; i <= 10; i ++) {
+            out += getRow(i) + "\n";
+        }
+        return out;
     }
 }
